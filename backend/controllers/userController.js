da871537a9ef96d3dfde67bdd56457fd7a9d348d
Sowrabh Shettigar <sowrabhshettigar114@gmail.com>
2025-08-10 23:04:22 +0530

    Add user model, controller, route, and API

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+import User from '../models/userModel.js';
+
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: 'Please fill all fields' });
+    }
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    if (user) {
+        return res.status(201).json(user);
+    } else {
+        return res.status(400).json({ message: 'Invalid user data' });
+    }
+};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const cors=require('cors');
 const dotenv=require('dotenv');
 const connectDB=require('./config/db');
+import userRoutes from '../routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -10,9 +11,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req, res)=>{
-    res.send('API is running...');
-});
+app.use('/api/user',userRoutes);
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
